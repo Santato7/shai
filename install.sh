6be2748b7dd0d720fdabcc5102b5427d144a1f36
Santato7 <santatolucas7@gmail.com>
2024-07-17 00:01:15 -0300

    chore: Update installation script and file paths for shai tool

--- install.sh
+++ install.sh
@@ -5,26 +5,25 @@ sudo -v
 echo "Installing the shai tool..."
 
 mkdir -p ~/.shai
-cd ~/.shai
 
 echo "Downloading the repository..."
-git clone https://github.com/santato7/shai.git . > /dev/null 2>&1-
+git clone https://github.com/santato7/shai.git ~/.shai/ > /dev/null 2>&1-
 
 echo "Creating virtual environment..."
-python3 -m venv venv > /dev/null 2>&1-
+python3 -m venv ~/.shai/venv > /dev/null 2>&1-
 
 echo "Activating virtual environment..."
-source venv/bin/activate > /dev/null 2>&1-
+source ~/.shai/venv/bin/activate > /dev/null 2>&1-
 
 echo "Installing dependencies..."
 pip install -r requirements.txt > /dev/null 2>&1-
 
-echo '#!/bin/bash' > shai
-echo './venv/bin/python3 main.py "$@"' >> shai
+echo '#!/bin/bash' > ~/.shai/shai
+echo '~/.shai/venv/bin/python3 ~/.shai/src/main.py "$@"' >> ~/.shai/shai
 
 deactivate > /dev/null 2>&1-
 
-chmod +x ./shai > /dev/null 2>&1-
+chmod +x ~/.shai/shai > /dev/null 2>&1-
 sudo ln -s ~/.shai/shai /usr/local/bin/shai > /dev/null 2>&1-
 
 echo "Installation complete!"
